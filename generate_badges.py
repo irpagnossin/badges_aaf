f519cea70346f2758c821482f2ce61af7b545dca
Ivan Ramos Pagnossin <ivan.pagnossin@gmail.com>
2016-05-21 20:56:23 -0300

    - GUI básica funcionando. Falta implementar um filechooser e layout

--- generate_badges.py
+++ generate_badges.py
@@ -15,70 +15,50 @@ reload(sys)
 sys.setdefaultencoding('utf8')
 locale.setlocale(locale.LC_TIME, 'pt_BR.utf8')
 
+def generate_badges(input_filename, badges_filename, secrets_filename, month, year):
 
 
-n_args = len(sys.argv)
+    pdf = canvas.Canvas(badges_filename)
+    pdf.setFillColor(cfg['COLOR'])
+    pdf.setFont(cfg['FONT_FAMILY'], cfg['FONT_SIZE'])
 
-# Define o nome do arquivo de entrada a partir dos argumentos
-if n_args >= 2:
-    input_filename = sys.argv[1]
-else:
-    raise Exception('Qual é o arquivo de entrada?')
+    SECRET_COL_NAME = 'Segredo'
 
-# Se o mês for dado como argumento (numérico: 1..12), usa-o;
-# se não, usa o mês seguinte ao atual.
-if n_args >= 3:
-    month = int(sys.argv[2])
-else:
-    month = datetime.date.today().month + 1
+    # Cria um DataFrame com os números de matrícula e nomes dados
+    # e escolhe aleatoriamente os segredos.
+    people = read_excel(input_filename, sheetname=0)
+    seed(int(year + month))
+    people[SECRET_COL_NAME] = Series(randint(0,100000,size=len(people)))
+    people.to_excel(secrets_filename, index=False)
 
-# Se o ano for dado como argumento (numérico), usa-o;
-# se não, usa o ano atual.
-if n_args >= 4:
-    year = int(sys.argv[3])
-else:
-    year = datetime.date.today().year
+    # Gera cada uma das imagens, de 4 em 4 associados
+    associates = []
+    count = 1
 
-# Define os nomes dos dois arquivos de saída: um contendo os crachás (PDF)
-# e o outro, os segredos (XLSX).
-badges_filename = "./output/%s-%s.pdf" % (year, month)
-secrets_filename = "./output/%s-%s_secrets.xlsx" % (year, month)
+    anualistas = month == 0
 
-pdf = canvas.Canvas(badges_filename)
-pdf.setFillColor(cfg['COLOR'])
-pdf.setFont(cfg['FONT_FAMILY'], cfg['FONT_SIZE'])
+    if anualistas:
+        month_name = str(year)
+    else:
+        month_name = "%s %s" % (datetime.date(year, month, 1).strftime('%B').upper(), str(year))
 
-SECRET_COL_NAME = 'Segredo'
+    for index, row in people.iterrows():
 
-# Cria um DataFrame com os números de matrícula e nomes dados
-# e escolhe aleatoriamente os segredos.
-people = read_excel(input_filename, sheetname=0)
-seed(int(year + month))
-people[SECRET_COL_NAME] = Series(randint(0,100000,size=len(people)))
-people.to_excel(secrets_filename, index=False)
+        associates.append({
+            'name'   : str(row['Nome']),
+            'title'  : str(row['Titulo']),
+            'secret' : row[SECRET_COL_NAME]
+        })
 
-# Gera cada uma das imagens, de 4 em 4 associados
-associates = []
-count = 1
-month_name = "%s %s" % (datetime.date(year, month, 1).strftime('%B').upper(), str(year))
+        if len(associates) == 4:
+            draw_page(pdf, associates, month_name)
+            pdf.showPage()
 
-for index, row in people.iterrows():
+            associates = []
+            count += 1
 
-    associates.append({
-        'name'   : str(row['Nome']),
-        'title'  : str(row['Titulo']),
-        'secret' : row[SECRET_COL_NAME]
-    })
-
-    if len(associates) == 4:
+    # Gera a ultima imagem, com 1, 2 ou 3 associados
+    if not len(associates) == 0:
         draw_page(pdf, associates, month_name)
-        pdf.showPage()
 
-        associates = []
-        count += 1
-
-# Gera a ultima imagem, com 1, 2 ou 3 associados
-if not len(associates) == 0:
-    draw_page(pdf, associates, month_name)
-
-pdf.save()
+    pdf.save()
--- main.py
+++ main.py
@@ -1,25 +1,90 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-from __future__ import print_function
-import easygui
-#path = easygui.fileopenbox()
-#print path
+import kivy
+from kivy.app import App
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.label import Label
+from kivy.uix.button import Button
+from kivy.uix.textinput import TextInput
+from kivy.uix.popup import Popup
+kivy.require('1.0.6') # replace with your current kivy version !
+import datetime
+import locale
 
-msg = "Preencha as informações abaixo"
-title = "Associação Atlética Floresta"
-fieldNames = ["Ano", "Mês"]
-fieldValues = easygui.multenterbox(msg, title, fieldNames)
-if fieldValues is None:
-    sys.exit(0)
-# make sure that none of the fields were left blank
-while 1:
-    errmsg = ""
-    for i, name in enumerate(fieldNames):
-        if fieldValues[i].strip() == "":
-          errmsg += "{} is a required field.\n\n".format(name)
-    if errmsg == "":
-        break # no problems found
-    fieldValues = multenterbox(errmsg, title, fieldNames, fieldValues)
-    if fieldValues is None:
-        break
-print("Reply was:{}".format(fieldValues))
+from generate_badges import generate_badges
+
+import sys
+
+reload(sys)
+sys.setdefaultencoding('utf8')
+locale.setlocale(locale.LC_TIME, 'pt_BR.utf8')
+
+class BadgesWindow(GridLayout):
+
+    def __init__(self, **kwargs):
+        super(BadgesWindow, self).__init__(**kwargs)
+        self.cols = 2
+
+        today = datetime.date.today()
+
+        #
+        self.add_widget(Label(text='Ano'))
+        self.year = TextInput(multiline=False)
+        self.add_widget(self.year)
+        self.year.text = str(today.year)
+
+        self.add_widget(Label(text='Mês'))
+        self.month = TextInput(multiline=False)
+        self.add_widget(self.month)
+        self.month.text = str(today.month + 1)
+
+        self.add_widget(Label(text='Arquivo de entrada'))
+        self.input_filename = TextInput(multiline=False)
+        self.add_widget(self.input_filename)
+        self.input_filename.text = './input/maio_2016_compl.xlsx'
+
+        self.generate_badges_btn = Button(text='Gerar crachás e segredos', font_size=14)
+        self.add_widget(self.generate_badges_btn)
+        self.generate_badges_btn.bind(on_press=self.gui_generate_badges)
+
+    def show_load(self, event):
+        content = LoadDialog(load=self.load, cancel=self.dismiss_popup)
+        self._popup = Popup(title="Escolha o arquivo", content=content,
+                            size_hint=(0.9, 0.9))
+        self._popup.open()
+
+    def dismiss_popup(self):
+        self._popup.dismiss()
+
+    def gui_generate_badges(self, event):
+
+        month = int(self.month.text)
+        year = int(self.year.text)
+
+        anualistas = month == 0
+
+        if anualistas:
+            badges_filename = "./%s.pdf" % year
+            secrets_filename = "./%s_secrets.xlsx" % year
+        else:
+            badges_filename = "./%s-%s.pdf" % (year, month)
+            secrets_filename = "./%s-%s_secrets.xlsx" % (year, month)
+
+        generate_badges(self.input_filename.text, badges_filename, secrets_filename, month, year)
+
+        message = 'Os seguintes arquivos foram gerados:\n%s: contém os crachás.\n%s: contém os segredos.' % (badges_filename, secrets_filename)
+
+        popup = Popup(title='Resumo',
+                content=Label(text=message),
+                size = (200,200),
+                auto_dismiss=False)
+        popup.open()
+
+class MyApp(App):
+
+    def build(self):
+        return BadgesWindow()
+
+
+if __name__ == '__main__':
+    MyApp().run()
